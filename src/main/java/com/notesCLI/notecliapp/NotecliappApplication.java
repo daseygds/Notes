8440296c3c0cee3dacace15b38672129f9c90a9d
Abhishek Das <abhishek.das11@gds.ey.com>
2026-04-07 19:34:20 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/notesCLI/notecliapp/NotecliappApplication.java
@@ -0,0 +1,13 @@
+package com.notesCLI.notecliapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotecliappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotecliappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notesCLI/notecliapp/NotecliappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notesCLI.notecliapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotecliappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
